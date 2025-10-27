869003db0c1d7aa73bf6116e5f84f87b38032b5e
Alexandr Kostiuchenko <x07.05.18@yandex.ru>
2025-10-27 20:52:41 +0300

    FZ16_2510272052

--- server/src/test/java/ru/practicum/shareit/IntegrationTest/ItemRequestDtoJsonTest.java
+++ server/src/test/java/ru/practicum/shareit/IntegrationTest/ItemRequestDtoJsonTest.java
@@ -23,37 +23,31 @@ class ItemRequestDtoJsonTest {
 
     @Test
     void testSerialize() throws Exception {
-        // given
         ItemRequestDto dto = new ItemRequestDto();
         dto.setId(1L);
         dto.setDescription("Нужна дрель");
         dto.setRequester(2L);
         dto.setCreated(LocalDateTime.of(2025, Month.OCTOBER, 25, 22, 0, 0));
 
-        // when
         String jsonStr = objectMapper.writeValueAsString(dto);
 
-        // then
         assertThat(jsonStr).contains("\"created\":\"2025-10-25T22:00:00\"");
         assertThat(jsonStr).contains("\"description\":\"Нужна дрель\"");
     }
 
     @Test
     void testDeserialize() throws Exception {
-        // given
         String jsonStr = """
-                {
+                  {
                   "id": 1,
                   "description": "Нужна дрель",
                   "requester": 2,
                   "created": "2025-10-25T22:00:00"
-                }
+                   }
                 """;
 
-        // when
         ItemRequestDto dto = objectMapper.readValue(jsonStr, ItemRequestDto.class);
 
-        // then
         assertThat(dto.getId()).isEqualTo(1L);
         assertThat(dto.getDescription()).isEqualTo("Нужна дрель");
         assertThat(dto.getRequester()).isEqualTo(2L);
